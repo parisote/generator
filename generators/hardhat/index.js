dab76de50896b77ea4eea3bb81cb20996b21328d
parisote <detomas@gmail.com>
2022-01-12 22:43:33 -0300

    fix 4 deprecated

--- generators/hardhat/index.js
+++ generators/hardhat/index.js
@@ -1,7 +1,12 @@
 const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
-  installDependencies() {
-    this.addDependencies(["hardhat", " @nomiclabs/hardhat-waffle", "ethereum-waffle", "chai", "@nomiclabs/hardhat-ethers", "ethers"]);
+  install() {
+    this.addDependencies({"hardhat": '^2.8.2'})
+    this.addDependencies({"@nomiclabs/hardhat-waffle": '^2.0.1'})
+    this.addDependencies({"chai":'^4.3.4'})
+    this.addDependencies({"ethereum-waffle":'^3.4.0'})
+    this.addDependencies({"@nomiclabs/hardhat-ethers":'^2.0.4'})
+    this.addDependencies({"ethers":'^5.5.3'})
   }
 };
