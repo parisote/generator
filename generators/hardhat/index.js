c7c9c471746c70169ea7f8d92a9df0fc4aee01d0
parisote <detomas@gmail.com>
2022-01-12 23:11:25 -0300

    template hardhat

--- generators/hardhat/index.js
+++ generators/hardhat/index.js
@@ -1,6 +1,10 @@
 const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
+  writing() {
+    this.fs.copy(this.templatePath("./"), this.destinationPath("./client"));
+  }
+
   install() {
     this.addDependencies({"hardhat": '^2.8.2'})
     this.addDependencies({"@nomiclabs/hardhat-waffle": '^2.0.1'})
@@ -8,5 +12,7 @@ module.exports = class extends Generator {
     this.addDependencies({"ethereum-waffle":'^3.4.0'})
     this.addDependencies({"@nomiclabs/hardhat-ethers":'^2.0.4'})
     this.addDependencies({"ethers":'^5.5.3'})
+    this.addDependencies({"lite-server": "^2.6.1"})
+    this.addDependencies({"bootstrap": "^5.1.3"})
   }
 };
